dd6df2ed3ede8d897fccc1122a7c415a500962ca
송준혁 <fprhqkrtk303@naver.com>
2024-05-17 14:27:34 +0900

    django: feat: Movie, Genre fixtures

--- django/movies/models.py
+++ django/movies/models.py
@@ -4,19 +4,23 @@ from django.conf import settings
 
 # Create your models here.
 class Movie(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
-    release_date = models.DateField()
+    original_title = models.CharField(max_length=100)
+    release_date = models.DateField(null=True)
     popularity = models.FloatField()
     vote_count = models.IntegerField()
     vote_average = models.FloatField()
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
+    backdrop_path = models.CharField(max_length=200, null=True)
     liked_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="liked_movies"
     )
 
 
 class Genre(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     movies = models.ManyToManyField(Movie, related_name="genres")
 
@@ -38,3 +42,5 @@ class Comment(models.Model):
     content = models.CharField(max_length=100)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
