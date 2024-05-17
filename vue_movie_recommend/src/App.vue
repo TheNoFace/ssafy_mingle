8aeb8abcabf3dda464ba153e779deb2fd3ff5867
김도영 <rlaehdud1002@naver.com>
2024-05-17 14:30:44 +0900

    vue: feat: login modal 추가

--- vue_movie_recommend/src/App.vue
+++ vue_movie_recommend/src/App.vue
@@ -1,13 +1,89 @@
 <template>
   <div>
-    
+    <nav>
+      <div class="nav-left">
+        <div>
+          <RouterLink :to="{ name: 'MainView' }">
+            <i
+              class="fa-solid fa-video p-3"
+              style="color: #76abae; height: 30px"
+            ></i>
+          </RouterLink>
+        </div>
+        <div class="nav-standard">
+          <RouterLink :to="{ name: 'CategoryView' }" class="nav-router"
+            >Category</RouterLink
+          >
+          <RouterLink :to="{ name: 'ReviewView' }" class="nav-router"
+            >Review</RouterLink
+          >
+        </div>
+      </div>
+      <div class="nav-right">
+        <button
+          type="button"
+          class="btn nav-router"
+          data-bs-toggle="modal"
+          data-bs-target="#login"
+        >
+          Login
+        </button>
+        <LoginModal />
+        <RouterLink :to="{ name: 'ProfileView' }">
+          <i
+            class="fa-solid fa-user p-3"
+            style="color: #76abae; height: 30px"
+          ></i>
+        </RouterLink>
+      </div>
+    </nav>
+
+    <div class="container">
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import { RouterLink } from "vue-router";
+import LoginModal from "@/component/LoginModal.vue";
 </script>
 
 <style scoped>
+* {
+  font-family: Pretendard;
+}
 
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-router {
+  color: white;
+  text-decoration: none;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: large;
+}
+
+.nav-left {
+  display: flex;
+}
+
+.nav-right {
+  display: flex;
+}
+
+.nav-standard {
+  display: flex;
+  align-items: center;
+}
+</style>
+
+<style>
+@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css");
+* {
+  background-color: #222831;
+}
 </style>
---
+++ vue_movie_recommend/src/component/LoginModal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="modal fade" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content modal-border-none">
+        <div class="modal-body modal-border">
+          <button type="button" class="btn close-btn" data-bs-dismiss="modal">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+          <!-- modal 상단 -->
+          <div class="modal-top">
+            <i class="fa-solid fa-video p-3 m-2" style="color: #76ABAE; height: 30px;"></i>
+            <h2 class="m-0 m-2" style="color: white;">영화 추천 서비스</h2>
+          </div>
+          <!-- 로그인 폼 -->
+          <h4 class="modal-login-text">로그인</h4>
+          <div class="login-form-outer">
+            <form>
+              <input class="form-control login-form-inner" type="text" placeholder="아이디를 입력하세요.">
+              <input class="form-control login-form-inner" type="password" placeholder="비밀번호를 입력하세요.">
+              <input class="form-control login-form-submit" type="submit" value="로그인">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+* {
+  color: white
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.modal-border-none {
+  border: none;
+}
+
+.modal-border {
+  border: 0.5px white solid;
+  border-radius: 10px;
+}
+
+.modal-top {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-login-text {
+  text-align: center;
+  margin: 40px 20px 0 0;
+}
+
+.login-form-outer {
+  margin: 15px 0;
+}
+
+.login-form-inner {
+  margin: 10px 0;
+  background-color: #222831;
+  color: white  ;
+}
+
+input::placeholder {
+  color: white;
+}
+
+.login-form-submit {
+  border: none;
+  margin: 10px 0;
+  background-color: #76ABAE;
+  color: white  ;
+}
+
+</style>
