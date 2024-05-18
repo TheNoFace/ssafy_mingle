db7a4b3b93d46061d7ee918678fc3844592d09c5
송준혁 <fprhqkrtk303@naver.com>
2024-05-18 18:05:54 +0900

    django: test: `django-extensions` 모듈 추가

--- django/movie_recommend/settings.py
+++ django/movie_recommend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'corsheaders',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
