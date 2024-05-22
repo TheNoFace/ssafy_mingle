6fd6ef700182a94201e5f39b57f115582db5a6da
김도영 <rlaehdud1002@naver.com>
2024-05-22 17:55:37 +0900

    django, vue: feat: 리뷰 생성, 삭제

--- vue_movie_recommend/src/router/index.js
+++ vue_movie_recommend/src/router/index.js
@@ -14,6 +14,7 @@ import ProfileGenre from "@/component/Profile/ProfileGenre.vue";
 import ProfileMovie from "@/component/Profile/ProfileMovie.vue";
 import ProfileReview from "@/component/Profile/ProfileReview.vue";
 import ProfileComment from "@/component/Profile/ProfileComment.vue";
+import ReviewFormView from "@/views/ReviewFormView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -96,6 +97,11 @@ const router = createRouter({
       name: "ReviewDetailView",
       component: ReviewDetailView,
     },
+    {
+      path: '/:tmdb_id/review',
+      name : "ReviewFormView",
+      component : ReviewFormView
+    }
   ],
 });
 
